e7490f9baee8c6fe52ba3f2ff58b1a9f005d05b8
tien-pan <tien0601@gms.tku.edu.tw>
2020-10-14 18:50:07 +0800

    Updates

---
+++ precache-manifest.b26580edccab647992f9c035b12c7a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e236614bde340c8f828c9bbead5d18be",
+    "url": "/1091-208410497-robotfriends/index.html"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/1091-208410497-robotfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a815ed9bc127da175c47",
+    "url": "/1091-208410497-robotfriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/1091-208410497-robotfriends/static/js/2.b52811a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-208410497-robotfriends/static/js/2.b52811a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a815ed9bc127da175c47",
+    "url": "/1091-208410497-robotfriends/static/js/main.76b3bcc3.chunk.js"
+  },
+  {
+    "revision": "4ad9d8e6756d89fc77b1",
+    "url": "/1091-208410497-robotfriends/static/js/runtime-main.78a163f3.js"
+  }
+]);
